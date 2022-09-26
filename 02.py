9070b0285d7e7c057d417a37e67b5179a926e3f2
ElanioJunior <112994446+ElanioJunior@users.noreply.github.com>
2022-09-26 08:15:15 -0300

    Update 02.py

--- 02.py
+++ 02.py
@@ -1,5 +1,18 @@
 nota1 = float(input('digite uma nota'))
 nota2 = float(input('digite uma nota'))
 nota3 = float(input('digite uma nota'))
-nota4 = (nota1 + nota2 + nota3) / 3
-print('a media final é:{}'.format(nota4))
+
+#Processamento de dados
+media = float(nota1 + nota2 + nota3) /3
+
+if 3 >= media >= 0:
+    print('a sua média de {} te deixa reprovado'.format(media))
+
+elif 5 >= media >= 4:
+    print('a sua média de {} te deixa em recuperação'.format(media))
+
+elif 10 >= media >= 6:
+    print('a sua média de {} te deia aprovado'.format(media))
+
+else:
+    print('a sua nota precisa ser entre 1 e 10')
