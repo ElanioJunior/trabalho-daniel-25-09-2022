276ee1e00be561bf486235fbc30dd3b5c1d5bc1c
ElanioJunior <112994446+ElanioJunior@users.noreply.github.com>
2022-09-26 08:23:43 -0300

    Update 04.py

--- 04.py
+++ 04.py
@@ -3,5 +3,19 @@ num2 = int(input('digite um numero: '))
 num3 = int(input('digite um numero: '))
 
 if num1 > num3 > num2:
-    print('a sequencie é {}>{}>{}'.format(num1, num3, num2))
-    
+    print('a sequencia é {}>{}>{}'.format(num1, num3, num2))
+
+elif num1 > num2 > num3:
+    print('a sequencia é {}>{}>{}'.format(num1, num2, num3))
+
+elif num2 > num1 > num3:
+    print('a sequencia é {}>{}>{}'.format(num2, num1, num3))
+
+elif num2 > num3 > num1:
+    print('a sequencia é {}>{}>{}'.format(num2, num3, num1))
+
+elif num3 > num2 > num1:
+    print('a sequencia é {}>{}>{}'.format(num3, num2, num1))
+
+elif num3 > num1 > num2:
+    print('a sequencia é {}>{}>{}'.format(num3, num1, num2))
