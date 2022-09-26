0f5235d889104066ac3b37581c475a9eb74ed93a
ElanioJunior <112994446+ElanioJunior@users.noreply.github.com>
2022-09-26 08:52:09 -0300

    Update 09.py

--- 09.py
+++ 09.py
@@ -1,5 +1,41 @@
-dia = str(input('digite um dia'))
-mes = str(input('digite um mês'))
-ano = str(input('digite um ano'))
+dia = int(input('digite um dia'))
+mes = int(input('digite um mês'))
+ano = int(input('digite um ano'))
+hora = str(input('digite a hora'))
+minuto = int(input('digite o minuto'))
 
-print('a data progamada é {}/{}/{}'.format(dia, mes, ano))
+if mes == 1:
+    print('{}/Janeiro/{} e {} horas e {} minutos'.format(dia, ano, hora, minuto))
+
+if mes == 2:
+    print('{}/Fevereiro/{} e {} horas e {} minutos'.format(dia, ano, hora, minuto))
+
+if mes == 3:
+    print('{}/Março/{} e {} horas e {} minutos'.format(dia, ano, hora, minuto))
+
+if mes == 4:
+    print('{}/Abril/{} e {} horas e {} minutos'.format(dia, ano, hora, minuto))
+
+if mes == 5:
+   print('{}/Maio/{} e {} horas e {} minutos'.format(dia, ano, hora, minuto))
+
+if mes == 6:
+   print('{}/Junho/{} e {} horas e {} minutos'.format(dia, ano, hora, minuto))
+
+if mes == 7:
+   print('{}/Julho/{} e {} horas e {} minutos'.format(dia, ano, hora, minuto))
+
+if mes == 8:
+   print('{}/Agosto/{} e {} horas e {} minutos'.format(dia, ano, hora, minuto))
+
+if mes == 9:
+   print('{}/Setembro/{} e {} horas e {} minutos'.format(dia, ano, hora, minuto))
+
+if mes == 10:
+   print('{}/Outubro/{} e {} horas e {} minutos'.format(dia, ano, hora, minuto))
+
+if mes == 11:
+   print('{}/Novembro/{} e {} horas e {} minutos'.format(dia, ano, hora, minuto))
+
+if mes == 12:
+   print('{}/Dezembro/{} e {} horas e {} minutos'.format(dia, ano, hora, minuto))
